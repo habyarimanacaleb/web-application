249fad56078b4bf06b9ed8ad7b36ca046ce30ada
Caleb <148393487+habyarimanacaleb@users.noreply.github.com>
2024-12-29 02:46:20 +0200

    update pages

--- pages/JS/logout.js
+++ pages/JS/logout.js
@@ -1,6 +1,6 @@
 async function logout() {
     try {
-      const response = await fetch('http://localhost:5000/logout', {
+      const response = await fetch('https://web-app-back-end.onrender.com/logout', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
